1870f28453cc5c241cd14ae211f7370332e69b2a
GrupoGps-Mecanizada <gpssa.mecanizada@gmail.com>
2025-04-23 14:41:10 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -12,7 +12,7 @@ const CONFIG = {
   API_KEY: 'AIzaSyAKitI_QG3Qd5v1DNXbAj_KwlNQSTUPRdY',
 
   // URL do Google Apps Script Web App
-  API_URL: 'https://script.google.com/macros/s/AKfycbwY7Eqi3HZzsxE9fh8ynFq9ldvHktWvNXuPm3xaA9EO6B4xLlsHqArh97hTQuY-yMCs/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzaTsp79vdni9Mnlj435aw5q9NcjMCGzbUc-wVTUtz3kxtsxWMiAQUROGhJ-IDrr4dy/exec',
 
   // Forçar login?
   AUTH_REQUIRED: false,
